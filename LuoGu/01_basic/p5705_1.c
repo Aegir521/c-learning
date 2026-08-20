af8a82c35c8496e9e48e88910201d354b887bd6f
Aegir521 <y9666099@gmail.com>
2026-08-20 23:25:25 +0800

    Add files via upload

---
+++ LuoGu/01_basic/p5705_1.c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double a;
+//	scanf("%lf", &a);
+//	printf("%d", (int)(a * 10) % 10);
+//	printf("%c", '.');
+//	printf("%d", (int)(a) % 10);
+//	printf("%d", (int)(a) / 10 % 10);
+//	printf("%d", (int)(a) / 10 / 10 % 10);
+//	return 0;
+//}
