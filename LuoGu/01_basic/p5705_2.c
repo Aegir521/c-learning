04d64963a749374564a7797e6f0ef9289e58613f
Aegir521 <y9666099@gmail.com>
2026-08-20 23:28:20 +0800

    Update p5705_2.c

--- LuoGu/01_basic/p5705_2.c
+++ LuoGu/01_basic/p5705_2.c
@@ -1,11 +1,11 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//int main()
-//{
-//	char a, b, c, d;
-//	scanf("%c%c%c.%c", &a, &b, &c, &d);
-//	printf("%c.%c%c%c", d, c, b, a);
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char a, b, c, d;
+	scanf("%c%c%c.%c", &a, &b, &c, &d);
+	printf("%c.%c%c%c", d, c, b, a);
+
+	return 0;
+}
